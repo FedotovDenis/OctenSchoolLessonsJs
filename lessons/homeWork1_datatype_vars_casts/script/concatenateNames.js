920f5f3354b39bc947a86d72a240f1c133953ece
denis <denisfedotov2107@gmail.com>
2024-04-29 23:52:16 +0300

    Homework done for the first lecture.

---
+++ lessons/homeWork1_datatype_vars_casts/script/concatenateNames.js
@@ -0,0 +1,16 @@
+/*
+Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+*/
+
+let firstName = 'Denis';
+
+let middleName = 'Olegovich';
+
+let lastName = 'Fedotov';
+
+
+
+let result = `${firstName} ${middleName} ${lastName} `;
+
+console.log(result);
---
+++ lessons/homeWork1_datatype_vars_casts/script/creatingVariables.js
@@ -0,0 +1,44 @@
+/*
+- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+Вивести кожну змінну за допомогою: console.log
+ */
+
+let greetings = 'hello';
+
+let str1 = 'owu';
+
+let str2 = 'com';
+
+let str3 = 'ua';
+
+let num1 = 1;
+
+let num2 = 10;
+
+let num3 = -999;
+
+let num4 = 123;
+
+let float1 = 3.14;
+
+let float2 = 2.7;
+
+const immutableNumber = 16;
+
+let bolian1 = true;
+
+let bolian2 = false;
+
+console.log(greetings);
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(float1);
+console.log(float2);
+console.log(immutableNumber);
+console.log(bolian1);
+console.log(bolian2);
